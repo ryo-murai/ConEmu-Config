1af89cdf860b67d8582195641073a137434f7c30
ryo-murai <murai.ry@gmail.com>
2013-09-13 17:40:49 +0900

    added readme and some comments

--- CreateConEmuShortCut.ps1
+++ CreateConEmuShortCut.ps1
@@ -1,3 +1,7 @@
+# create a shortcut to launch conemu with conemu.xml located here.
+#  * command line options are
+#    https://code.google.com/p/conemu-maximus5/wiki/Command_Line
+
 $currDir = Split-Path $MyInvocation.MyCommand.Path
 $conEmuDir = "C:\Program Files\ConEmu\"
 
@@ -10,4 +14,3 @@ $shortcut.Description = "Console Emulator (x86)"
 $shortcut.IconLocation = "$conEmuDir\ConEmu.exe, 0"
 $shortcut.WindowStyle = 1
 $shortcut.Save()
-
---
+++ Readme.md
@@ -0,0 +1,9 @@
+ConEmu-Config
+=============
+
+My ConEmu settings.
+
+## Usage
+1. first time, run `CreateConEmuShortCut.ps1`
+1. launch the generated shortcut `ConEmu.lnk`
+1. pin running ConEmu for next use
