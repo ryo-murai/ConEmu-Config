59b8917e1483a0f721e2756119748958f381288d
ryo-murai <murai.ry@gmail.com>
2013-09-17 09:52:55 +0900

    support conemu64.exe in 64bit env

--- CreateConEmuShortCut.ps1
+++ CreateConEmuShortCut.ps1
@@ -1,16 +1,17 @@
-# create a shortcut to launch conemu with conemu.xml located here.
-#  * command line options are
+# this script creates a shortcut for ConEmu to run with conemu.xml in the current directory.
+#  * refer to ConEmu command line options:
 #    https://code.google.com/p/conemu-maximus5/wiki/Command_Line
 
 $currDir = Split-Path $MyInvocation.MyCommand.Path
 $conEmuDir = "C:\Program Files\ConEmu\"
+$exePath = Get-ChildItem $conEmuDir -filter *.exe | select -first 1 | % {$_.FullName}
 
 $shell = New-Object -COM WScript.Shell
 $shortcut = $shell.CreateShortcut("$currDir\ConEmu.lnk")
-$shortcut.TargetPath = "$conEmuDir\ConEmu.exe"
+$shortcut.TargetPath = $exePath
 $shortcut.Arguments = "/LoadCfgFile $currDir\ConEmu.xml"
 $shortcut.WorkingDirectory = "$conEmuDir"
-$shortcut.Description = "Console Emulator (x86)"
-$shortcut.IconLocation = "$conEmuDir\ConEmu.exe, 0"
+$shortcut.Description = "Console Emulator"
+$shortcut.IconLocation = "$exePath, 0"
 $shortcut.WindowStyle = 1
 $shortcut.Save()
